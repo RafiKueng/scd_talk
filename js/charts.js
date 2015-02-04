802369c35840b239a544aa6f71396a74bf7db31a
Rafael Küng <rafik@physik.uzh.ch>
2015-02-04 21:50:06 +0100

    building slides

--- js/charts.js
+++ js/charts.js
@@ -1,11 +1,15 @@
 
-Reveal.addEventListener('slide1', function () {
-    initSlide1();
-});
+/**
+ * Assign handlers
+ */ 
+Reveal.addEventListener('slide1', function () { initSlide1(); });
+Reveal.addEventListener('treeslide', function () { initTree(); initTree = function(){};});
 
-Reveal.addEventListener('treeslide', function () { initTree(); });
 
 
+/**
+ * Functions
+ */
 var initTree = function () {
     var width = 800,
         height = 1500;
@@ -19,34 +23,34 @@ var initTree = function () {
     var svg = d3.select("#tree_container").append("svg")
         .attr("width", width)
         .attr("height", height)
-      .append("g")
+        .append("g")
         .attr("transform", "translate(40,0)");
 
     d3.json("data/data.json", function(error, json) {
-      var nodes = tree.nodes(json),
-          links = tree.links(nodes);
+        var nodes = tree.nodes(json),
+            links = tree.links(nodes);
 
-      var link = svg.selectAll("path.link")
-          .data(links)
-        .enter().append("path")
-          .attr("class", "link")
-          .attr("d", diagonal);
+        var link = svg.selectAll("path.link")
+            .data(links)
+            .enter().append("path")
+            .attr("class", "link")
+            .attr("d", diagonal);
 
-      var node = svg.selectAll("g.node")
-          .data(nodes)
-        .enter().append("g")
-          .attr("class", "node")
-          .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
+        var node = svg.selectAll("g.node")
+            .data(nodes)
+            .enter().append("g")
+            .attr("class", "node")
+            .attr("transform", function(d) { return "translate(" + d.y + "," + d.x + ")"; })
 
-      node.append("circle")
-          .attr("r", 4.5);
+        node.append("circle")
+            .attr("r", 4.5);
 
-      node.append("text")
-          .attr("dx", function(d) { return d.children ? -8 : 8; })
-          .attr("dy", 3)
-          .attr("text-anchor", function(d) { return d.children ? "end" : "start"; })
-          .text(function(d) { return d.name; });
-    });
+        node.append("text")
+            .attr("dx", function(d) { return d.children ? -8 : 8; })
+            .attr("dy", 3)
+            .attr("text-anchor", function(d) { return d.children ? "end" : "start"; })
+            .text(function(d) { return d.name; });
+        });
 
     d3.select(self.frameElement).style("height", height + "px");
 };
